7885e1e1bcd279621d8f8e0a58d2b36fd77406a3
Judy <97323501@qq.com>
2017-10-14 13:46:28 +0800

    add  a activity

---
+++ app/src/main/java/com/shl/song/powertest/PowerTest.java
@@ -0,0 +1,39 @@
+package com.shl.song.powertest;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Administrator on 2017/10/14 0014.
+ */
+
+public class PowerTest extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
